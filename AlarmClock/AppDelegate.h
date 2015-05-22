0385a413c6bace85b4cddd28d9864b91c8612df1
Harris Osserman <harris.osserman@gc.com>
2015-05-22 10:30:37 -0400

    Initial Commit

---
+++ AlarmClock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlarmClock
+//
+//  Created by Harris Osserman on 5/22/15.
+//  Copyright (c) 2015 Harris Osserman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlarmClock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlarmClock
+//
+//  Created by Harris Osserman on 5/22/15.
+//  Copyright (c) 2015 Harris Osserman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
