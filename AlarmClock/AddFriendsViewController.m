e86c0ebc1a2e23caac95be6614df4113dca63b31
Harris Osserman <harris.osserman@gc.com>
2015-06-04 12:39:57 -0700

    upload contacts and do error handling

--- AlarmClock/AddFriendsViewController.m
+++ AlarmClock/AddFriendsViewController.m
@@ -16,14 +16,33 @@
     
     [contacts startContactsUploadWithCompletion:^(DGTContactsUploadResult *result, NSError *error) {
         if (error) {
-            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error with contacts upload"
-                                                            message:error.description
-                                                           delegate:nil
-                                                  cancelButtonTitle:@"OK"
-                                                  otherButtonTitles:nil];
-            [alert show];
+            [self showPopup:[error description] withTitle:@"Error with contacts upload"];
+        } else {
+            [self findContactMatches];
         }
     }];
+}
+
+- (void)findContactMatches {
+    DGTSession *userSession = [Digits sharedInstance].session;
+    DGTContacts *contacts = [[DGTContacts alloc] initWithUserSession:userSession];
+    
+    [contacts lookupContactMatchesWithCursor:nil completion:^(NSArray *matches, NSString *nextCursor, NSError *error) {
+        if (error) {
+            [self showPopup:[error description] withTitle:@"Find friends error"];
+        } else if ([matches count] == 0) {
+            [self showPopup:@"You have no contacts using this app :(" withTitle:@"No contacts found"];
+        }
+    }];
+}
+
+- (void)showPopup:(NSString *)message withTitle:(NSString *)title{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                    message:message
+                                                   delegate:nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
 }
 
 @end
