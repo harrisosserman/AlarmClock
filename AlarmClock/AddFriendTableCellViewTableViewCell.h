4eb1acb6dc198e31b8db9760b7c925f9ca27320b
Harris Osserman <harris.osserman@gc.com>
2015-06-14 14:22:10 -0400

    table cell WIP

---
+++ AlarmClock/AddFriendTableCellViewTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  AddFriendTableCellViewTableViewCell.h
+//  AlarmClock
+//
+//  Created by Harris Osserman on 6/14/15.
+//  Copyright (c) 2015 Harris Osserman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddFriendTableCellViewTableViewCell : UITableViewCell
+
+@end
---
+++ AlarmClock/AddFriendTableCellViewTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  AddFriendTableCellViewTableViewCell.m
+//  AlarmClock
+//
+//  Created by Harris Osserman on 6/14/15.
+//  Copyright (c) 2015 Harris Osserman. All rights reserved.
+//
+
+#import "AddFriendTableCellViewTableViewCell.h"
+
+@implementation AddFriendTableCellViewTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
